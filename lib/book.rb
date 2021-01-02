6394319be9580bdae775b150cbb1988745b51ef1
Keith Filkins <kafilkins@me.com>
2021-01-02 23:32:47 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,8 +1,8 @@
 class Book
   attr_accessor :author, :page_count
-  attr_reader :title, :genre
+  attr_reader :title, :genres
 
-  GENRES = []
+GENRES = []
 
   def initialize(title)
     @title = title
